087b0a4795fab2dc07c86b07fdec2b806aa47424
KarthikeyaVoocha <131981842+KarthikeyaVoocha@users.noreply.github.com>
2025-03-29 12:26:01 +0530

    Create CommandLineDemo2.java

---
+++ CommandLineDemo2.java
@@ -0,0 +1,13 @@
+class CommandLineDemo2{
+public static void main(String args[]){
+String name=args[0];
+String rollno = args[1];
+String Dept = args[2];
+
+
+
+System.out.println("name"+name);
+System.out.println("rollno"+rollno);
+System.out.println("Dept"+Dept);
+}
+}
